2fd1805b8386eb4bdad25a51f310de4344e53328
Juan Castillo <castillo10961@gmail.com>
2021-08-09 22:40:48 -0500

    main

---
+++ components/Result.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Result = () =>{
+    return (
+        <div className="result">
+            <span>0</span>
+        </div>
+    )
+}
+
+export default Result
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 //Importacion
-import React from 'react';
+import React from 'react'
+import Result from '../components/Result'
+import './App.css'
 
 //Generacion de la funcion del componente
 const App = ()=> {
+  
     //Lo que ejecuta la funcion
-    return <h1> Calc App probando guthub </h1>
+    return (
+    <main className='react-calculator'>
+      <result> </result>
+     <div className="numbers">
+       <button>1</button>
+       <button>2</button>
+       <button>3</button>
+       <button>4</button>
+       <button>5</button>
+       <button>6</button>
+       <button>7</button>
+       <button>8</button>
+       <button>9</button>
+       <button>10</button>
+       </div>
+       <div className="function">
+         <button>Clear</button>
+         <button>r</button>
+         </div>
+         <div className="math-operation">
+           <button>+</button>
+           <button>-</button>
+           <button>*</button>
+           <button>/</button>
+           <button>=</button>
+           </div>
+
+
+  
+    </main>)
 }
 
 //Exportancion
