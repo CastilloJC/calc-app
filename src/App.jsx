f91e3ae978d20fa12d1d06a0cf4911baaf1e0b7b
Juan Castillo <castillo10961@gmail.com>
2021-08-09 19:37:07 -0500

    main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//Importacion
+import React from 'react';
+
+//Generacion de la funcion del componente
+const App = ()=> {
+    //Lo que ejecuta la funcion
+    return <h1> Calc App </h1>
+}
+
+//Exportancion
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+//Importar el componente
+
+//Utilizar el componente
+ReactDOM.render( <App/>,
+    document.getElementById('root'))
