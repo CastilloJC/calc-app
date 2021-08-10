6852a2fabadfccecc241989030bffa3ceb6bb446
Juan Castillo <castillo10961@gmail.com>
2021-08-09 19:57:18 -0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 //Generacion de la funcion del componente
 const App = ()=> {
     //Lo que ejecuta la funcion
-    return <h1> Calc App </h1>
+    return <h1> Calc App probando guthub </h1>
 }
 
 //Exportancion
